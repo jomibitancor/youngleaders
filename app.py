9f8f10bc42fcface23d98183fd1e5bf4bb0a142a
John Bitancor <bitancor@ualberta.ca>
2020-04-16 10:10:12 -0600

    courses page

--- app.py
+++ app.py
@@ -48,5 +48,9 @@ def opportunities():
 def contact():
     return render_template('page08_contact.html')
 
+@app.route('/courses')
+def courses():
+    return render_template('page09_courses.html')
+
 if __name__ == '__main__':
  app.run(debug=True)
