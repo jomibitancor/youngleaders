43459402f760a22093744394b0fbd441509c9d72
jomi <bitancor@ualberta.ca>
2020-05-31 15:45:11 -0600

    main page done 1 hour

--- static/js/main.js
+++ static/js/main.js
@@ -1,52 +1,49 @@
-const nav = document.getElementById('nav-toggler');
+const nav = document.getElementById("nav-toggler");
 const navListener = nav.addEventListener("click", navToggle);
 const mySidenav = document.getElementById("mySidenav");
 
-if (window.innerWidth < 994){
-    mySidenav.innerHTML = `
+if (window.innerWidth < 994) {
+  mySidenav.innerHTML = `
         <a href="/events">Events</a>
-        <a href="/videoresources">Video Resources</a>
+        <a href="https://youngleaders-world.mykajabi.com/opt-in">Video Resources</a>
         <a href="/courses">Courses</a>
         <a href="/speakers">Speakers</a>
         <a href="/waystogive">Ways To Give</a>
         <a href="/about">About Us</a>
         <a href="/opportunities">Opportunities</a>
         <a href="/contact">Contact Us</a>
-    `
+    `;
 }
 
-let linkItems = mySidenav.getElementsByTagName('a');
-linkItems = Array.from(linkItems)
+let linkItems = mySidenav.getElementsByTagName("a");
+linkItems = Array.from(linkItems);
 
 var x = 0;
-function navToggle(){
-    if(x==0){
-        x = 1;
-        openNav()
-    }
-    else{
-        x = 0;
-        closeNav()
-    }
+function navToggle() {
+  if (x == 0) {
+    x = 1;
+    openNav();
+  } else {
+    x = 0;
+    closeNav();
+  }
 }
 function openNav() {
-    mySidenav.style.width = "250px";   
-    linkItems.forEach(link => {
-        link.classList.add("fadeIn")
-        // setTimeout(() => {
-        //     link.classList.remove("fade")
-        // }, 1000);
-    });
-    
+  mySidenav.style.width = "250px";
+  linkItems.forEach((link) => {
+    link.classList.add("fadeIn");
+    // setTimeout(() => {
+    //     link.classList.remove("fade")
+    // }, 1000);
+  });
 }
 function closeNav() {
-    mySidenav.style.width = "0px";   
-    linkItems.forEach(link => {
-        link.classList.remove("fadeIn")
-        link.classList.add("fadeout")
-        setTimeout(() => {
-            link.classList.remove("fadeout")
-        }, 1000);
-    });
+  mySidenav.style.width = "0px";
+  linkItems.forEach((link) => {
+    link.classList.remove("fadeIn");
+    link.classList.add("fadeout");
+    setTimeout(() => {
+      link.classList.remove("fadeout");
+    }, 1000);
+  });
 }
-
